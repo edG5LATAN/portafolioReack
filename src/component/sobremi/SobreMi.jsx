9a2f4fd4b243b13291ff9a6063afb48ae2d670c6
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2024-07-09 22:10:15 -0600

    Update SobreMi.jsx
    se agrego texto nuevo

--- src/component/sobremi/SobreMi.jsx
+++ src/component/sobremi/SobreMi.jsx
@@ -8,10 +8,10 @@ function SobreMi() {
         <h1 className='sobremi-titulo'>Acerca de mi</h1>
         <p className='sobremi-p' >Mi nombre es Edwin Castro soy egresado de Infop con conocimineto
           en electronica en general y electronica en computacion trabaje con
-          electronica en telefonia de ahi me fue gustando el mundo de la 
-          programacion donde ahora ya creamos varios programas uno de ellos es
+          electronica en telefonia donde adquiri conocimiento en programacion 
+          donde ahora ya creamos varios proyectos uno de ellos es
           esta pagina de portafolio con algunos trabajos echos hasta la fecha
-          gracias ala ayuda de ORACLE y ALURA LATAM y esperamos seguir aprendiendo
+          gracias ala ayuda de ORACLE, ALURA LATAM, UDEMY, JAB...y continuo aprendiendo
           y seguir avanzando en un mundo de tecnologia donde crece mas dia a dia.
         </p>
         <p className='sobremi-firma'>Edwin Castro</p>
